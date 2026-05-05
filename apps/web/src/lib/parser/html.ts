804fa237cb8358500a3f6894f7452b27ca32e576
Jiyong Youn <01@0101010101.com>
2026-05-06 08:49:45 +0900

    fix(web): 🛡️ strengthen HTML event handler sanitization regex (C22-SEC01)

--- apps/web/src/lib/parser/html.ts
+++ apps/web/src/lib/parser/html.ts
@@ -36,8 +36,8 @@ export function normalizeHTML(html: string): string {
     .replace(/<(iframe|object|embed)[\s\S]*?<\/\1>/gi, '')
     .replace(/<(iframe|object|embed)[^>]*>/gi, '')
     // Remove event handler attributes (onclick, onerror, etc.)
-    .replace(/\son\w+=["'][^"']*["']/gi, '')
-    .replace(/\son\w+=\w+/gi, '')
+    // Handles quoted, unquoted, and parenthesized values (C22-SEC01)
+    .replace(/\son\w+=[^>\s]*/gi, '')
     // Fix malformed closing tags
     .replace(/<\/(td|th|tr|table|thead|tbody)\s+>/gi, '</$1>')
     .replace(/<\/([a-z][a-z0-9]*)\s+>/gi, '</$1>');
